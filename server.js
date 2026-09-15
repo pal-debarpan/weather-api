a3977093a2f8ccaa2f6a34326078c4f62f15cae9
Debarpan Pal <debarpan32008@gmail.com>
2026-09-15 20:53:02 +0530

    Add weather query parameter route

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ app.listen(3000);
 app.get("/", (req,res) => {
     res.send("Hello from Weather API");
 });
+
+app.get("/weather", (req,res) => {
+    const city = req.query.city;
+    res.send(`Weather requested for ${city}`);
+});
